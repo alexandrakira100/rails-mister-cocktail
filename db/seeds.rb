0daea2f404c2a5ace950727c37356a17f3bf79de
Alexandra Doll <alexandra.doll100@gmail.com>
2018-05-25 16:27:50 +0900

    over it

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,21 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+Ingredient.destroy_all if Rails.env.delevopment?
+
+require 'json'
+require 'open-uri'
+
+puts "opening url"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+list_serialized = open(url).read
+list = JSON.parse(list_serialized)
+list['drinks'].each do |ingredient|
+ print "."
+ Ingredient.create name: ingredient['strIngredient1']
+end
+puts
+puts "completed seed"
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
